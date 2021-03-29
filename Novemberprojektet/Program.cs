b6905de753891957b5af6e279ff4ed406ab347c5
Mackapacka_15 <Markuspiotrowski@live.com>
2021-03-29 09:38:32 +0200

    Experimentation

--- Novemberprojektet/Program.cs
+++ Novemberprojektet/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using Raylib_cs;
+using System.Numerics;
 
+//Skrev fel när jag skapade Sulutionen därför heter den Novemberprojektet istället för Slutprojekt
 namespace Novemberprojektet
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Raylib.InitWindow(700, 700, "Asteroids");
+            Raylib.SetTargetFPS(60);
+            int height = Raylib.GetScreenHeight();
+            int width = Raylib.GetScreenWidth();
+
+
+
+
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Vector2 Mousepos = new Vector2(Raylib.GetMouseX(), Raylib.GetMouseY());
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.BLACK);
+
+                Raylib.DrawRectangle(1, 1, 10, 10, Color.WHITE);
+
+                Raylib.EndDrawing();
+
+
+            }
         }
     }
 }
